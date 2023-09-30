9a0db8c197bdbba6c26e75d76eb7ddb65ce697c2
kazlo1290 <69760790+kazlo1290@users.noreply.github.com>
2023-09-30 18:21:21 +0800

    First commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,7 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.sass']
 })
 export class AppComponent {
-  title = 'angular-app';
+  title = 'angular-app ----sdfsd';
+  message = 'Hello world!';
+  clicked = false;
 }
